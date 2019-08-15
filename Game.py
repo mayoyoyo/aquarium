82f672b57c39f1c81b42483d02f1c8d02a9edb84
Hanson Kang <Hanson@Hansons-MacBook-Pro.local>
2019-08-15 00:28:44 -0700

    comparehands takes player as args and game updated

--- Game.py
+++ Game.py
@@ -7,13 +7,15 @@ from comparehands import compare
 
 class Game:
 
-	def __init__(self, table):
+	def __init__(self, table, sb):
 		self.hand_no = 0
 		self.table = table
 		self.players = []
-		self.button = 0
+		self.button = 0 
+		self.sb = sb
 
-	#check how many players have enough chips to play
+	#check how many players have enough chips to play and updates players in game
+	#returns number of players with chips remaining
 	def update(self):
 		n = 0
 		self.players = self.table.players[:]
@@ -23,30 +25,71 @@ class Game:
 			else: n += 1
 		return n
 
-
+	#begin the game
 	def start(self):
-		#if there are enough players with chips
-		if self.update() > 1:
+		rollover = 0
+		#if there are enough players with chips keep running hands
+		while self.update() > 1:
+
 			self.hand_no += 1
-			hand = Hand(self.players, self.button)
+			
+			# in the beginning button is lowest indexed player to have enough chips
+			if self.hand_no == 1:
+				for i in range(len(self.players)):
+					if self.players[i].chips > 0:
+						self.button = i
+						break
+			else:
+				#consider patching when bb and/or sb leaves, but for now just move once
+				for i in range(len(self.players)):
+					self.button += 1
+					if self.players[(self.button) % len(self.players)].chips > 0:
+						self.button = (self.button) % len(self.players)
+						break
+
+			hand = Hand(self.players, self.button, self.sb, rollover)
+			rollover = 0
+			hand.dealplayers()
+			hand.post()
+			
+			#testing
+			print("Pot:% d" %(hand.pot))
+
+			hand.dealflop()
+	   		hand.dealturn()
+	   		hand.dealriver()
+	   		print 
+	   		print hand.board
+	   		print 
+	   		print("Winners: ")
+	   		rollover = hand.showdown()
+
+			#testing
+			prompt = raw_input("\n" + "Next hand? (Y/N) ")
+   			if prompt == "y": 
+   				continue
+   			else: break
 
 
 
 
 def main():
 	p1 = Player("Hero")
-	p2 = Player("Villain")
-	p3 = Player("button")
+	p2 = Player("Vill")
+	p3 = Player("Fish")
    	table = Table()
    	table.sit(p1, 0)
    	table.sit(p2, 1)
 
-   	game = Game(table)
+   	game = Game(table, 1)
    	
 	table.sit(p3, 2)
-	p1.getchips(20)
-   	p2.getchips(20)
-   	game.start()
+	p1.getchips(200)
+   	p2.getchips(200)
+   	p3.getchips(200)
+   	prompt = raw_input("Start? (Y/N) ")
+   	if prompt == "y": 
+   		game.start()
    	
 	#print game.table.players[1].hand
 	
--- comparehands.py
+++ comparehands.py
@@ -1,32 +1,35 @@
 from gethand import evaluate
+from Player import Player 
 import sys 
 
 #compare two hands on given board and return winner(s) as a list
 
-def compare(board, hole1, hole2):
-	cards1 = board + hole1
-	cards2 = board + hole2
+def compare(board, p1, p2):
+	cards1 = board + p1.hand
+	cards2 = board + p2.hand
 
 	hand1 = evaluate(cards1)
 	hand2 = evaluate(cards2)
 
 	if hand1[0] > hand2[0]: 
-		return [hole1]
+		return [p1]
 	if hand2[0] > hand1[0]:
-		return [hole2]
+		return [p2]
 
 	if hand1 == hand2:
-		return [hole1, hole2]
+		return [p1, p2]
 
 	#if index is same and not a split
-	if hand1[1] > hand2[1]: return [hole1]
-	else: return [hole2]
+	if hand1[1] > hand2[1]: return [p1]
+	else: return [p2]
 
 def main():
     
-    board = ["Kd", "Ks", "Ad", "4c", "7s"]
-    hero = ["Kh", "As"]
-    villain = ["Ah", "Ac"]
+    board = ['6s', '2s', '2d', '2c', '6d']
+    hero = Player("Hero")
+    villain = Player("Vill")
+    hero.hand = ["Jh", "Ac"]
+    villain.hand = ["7s", "Jd"]
     print compare(board, hero, villain)
 
 if __name__ == '__main__':
